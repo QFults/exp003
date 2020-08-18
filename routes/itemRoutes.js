b09e4db9a06e48c9a22895436de6a4eb54f9a7e3
Quinton Fults <quinton.fults.work@gmail.com>
2020-08-18 14:18:56 -0700

    code

---
+++ routes/itemRoutes.js
@@ -0,0 +1,69 @@
+const router = require('express').Router()
+const { join } = require('path')
+const fs = require('fs')
+
+// GET all items
+router.get('/items', (req, res) => {
+  fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+    res.json(JSON.parse(data))
+  })
+})
+
+// POST one item
+router.post('/items', (req, res) => {
+
+  fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+
+    let items = JSON.parse(data)
+    items.push(req.body)
+
+    fs.writeFile(join(__dirname, '..', 'db', 'db.json'), JSON.stringify(items), err => {
+      if (err) { console.log(err) }
+
+      res.sendStatus(200)
+    })
+  })
+})
+
+// PUT one item
+router.put('/items/:text', (req, res) => {
+
+  fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+
+    let items = JSON.parse(data)
+
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].text === req.params.text) {
+        items[i].isDone = req.body.isDone
+      }
+    }
+
+    fs.writeFile(join(__dirname, '..', 'db', 'db.json'), JSON.stringify(items), err => {
+      if (err) { console.log(err) }
+
+      res.sendStatus(200)
+    })
+  })
+})
+
+// DELETE one item
+router.delete('/items/:text', (req, res) => {
+
+  fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+
+    let items = JSON.parse(data)
+    items = items.filter(item => item.text !== req.params.text)
+
+    fs.writeFile(join(__dirname, '..', 'db', 'db.json'), JSON.stringify(items), err => {
+      if (err) { console.log(err) }
+
+      res.sendStatus(200)
+    })
+  })
+})
+
+module.exports = router
